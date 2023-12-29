35bbfde6c60397cbe02e194b569bd0b4a089910b
Rob <balian.rob@gmail.com>
2023-12-29 13:54:59 -0800

    woo

--- main.py
+++ main.py
@@ -1,18 +1,65 @@
 from fastapi import FastAPI, File, UploadFile, Form
 from fastapi.responses import FileResponse, HTMLResponse
+from dotenv import load_dotenv
 import os
 import fitz  # PyMuPDF
 import shutil
+from openai import OpenAI
+import json
+
+load_dotenv()
 
 app = FastAPI()
+openaiClient = OpenAI()
 
 # Directory where files will be saved
 FILE_DIR = "uploaded_files"
 if not os.path.exists(FILE_DIR):
     os.makedirs(FILE_DIR)
 
-def redact_pdf(input_pdf, output_pdf, word_to_redact):
+def get_redaction_strings(page_text, redaction_instructions):
+  try:
+      response = openaiClient.chat.completions.create(
+          model= "gpt-4-1106-preview", #"gpt-4-1106-preview", #"gpt-3.5-turbo-1106"
+          response_format={ "type": "json_object" },
+          messages=[
+              {"role": "system", "content": "You are a helpful JSON-outputting redaction assistant. You output a JSON array with the exact strings (which may include spaces or weird characters) to redact from a PDF. "},
+              {"role": "user", "content": 'Please identify the strings to redact based on these instructions: ' + redaction_instructions + \
+               '\n Note: ONLY make these redactions. It\'s very important to not OVER-redact information. \
+                You should NOT redact information that should be public (the clerks office, the attorneys trying the case, the recipient of the letter)\
+                \n\n Here is the text: '+page_text+' \n\n reponse format: {"strings_to_redact": ["string1", "string2"]}'}
+          ]
+      )
+      print(response)
+      jsonResponse = json.loads(response.choices[0].message.content)
+      return jsonResponse['strings_to_redact']  # Adjust according to the expected format
+  except Exception as e:
+      print(f"Error: {e}")
+      return None
+
+def redact_pdf(input_pdf, output_pdf, redact_instructions):
     doc = fitz.open(input_pdf)
+
+
+
+    # Iterate through each page
+    full_text = ''
+    for page in doc:
+      # fitz.TEXT_INHIBIT_SPACES
+      # search default flags (TEXT_PRESERVE_LIGATURES | TEXT_PRESERVE_WHITESPACE | TEXT_MEDIABOX_CLIP | TEXT_DEHYPHENATE)
+      flags = (fitz.TEXT_PRESERVE_LIGATURES | fitz.TEXT_INHIBIT_SPACES | fitz.TEXT_PRESERVE_WHITESPACE | fitz.TEXT_MEDIABOX_CLIP | fitz.TEXT_DEHYPHENATE)
+      pageText = page.get_text("text", flags=flags)
+      full_text += pageText
+
+
+    stringsToRedact = get_redaction_strings(full_text, redact_instructions)      
+    print('stringsToRedact', stringsToRedact)
+
+    if stringsToRedact is None:
+      print('error getting strings to redact')
+      return
+
+
 
     # Iterate through each page
     for page in doc:
@@ -20,33 +67,27 @@ def redact_pdf(input_pdf, output_pdf, word_to_redact):
       # search default flags (TEXT_PRESERVE_LIGATURES | TEXT_PRESERVE_WHITESPACE | TEXT_MEDIABOX_CLIP | TEXT_DEHYPHENATE)
       flags = (fitz.TEXT_PRESERVE_LIGATURES | fitz.TEXT_INHIBIT_SPACES | fitz.TEXT_PRESERVE_WHITESPACE | fitz.TEXT_MEDIABOX_CLIP | fitz.TEXT_DEHYPHENATE)
       textPage = page.get_textpage(flags=flags)
-      pageText = page.get_text("words", flags=flags)
-      concatenatedText = ' '.join(list(map(lambda x: x[4], pageText)))
+      pageText = page.get_text("text", flags=flags)
 
-      #print(pageText)
-      text_instances = page.search_for(word_to_redact, flags=flags)
-      #combined = combineNearbyRects(text_instances)
-      #combined = [fitz.Rect(254.8533172607422, 475.08038330078125, 295.45037841796875, 484.93927001953125)]
-      
-      #print('string match occurances', len(re.findall(word_to_redact.lower(), pageText.lower())))
-      print('search_for instances', len(text_instances))
-      #print('combined instances', len(combined))
+      for stringToRedact in stringsToRedact:
+        text_instances = page.search_for(stringToRedact, flags=flags, textpage=textPage)
+        print(text_instances)
+        print(f'redacting {stringToRedact} with {len(text_instances)} instances')
 
-
-      # Iterate through each found instance and create a redaction annotation
-      for inst in text_instances:
-        #print(inst)
-        redact = page.add_redact_annot(inst, text='', fill=(0,0,0)) # .join(['X' for l in redact_word]))  # Replace text with a blank space
-        redact.update()
+        # Iterate through each found instance and create a redaction annotation
+        for inst in text_instances:
+          #print(inst)
+          redact = page.add_redact_annot(inst, text='', fill=(0,0,0)) # .join(['X' for l in redact_word]))  # Replace text with a blank space
+          redact.update()
 
     # # Apply the redactions
     for p in doc.pages():
         p.apply_redactions()
-    #doc.apply_redactions()
 
     # Save the redacted document
     doc.save(output_pdf)
     doc.close()
+    return {"filename": output_pdf, 'redacted_strings': stringsToRedact}
 
 # Serve the static HTML file
 @app.get("/", response_class=HTMLResponse)
@@ -65,11 +106,11 @@ async def upload_pdf(file: UploadFile = File(...), redact_instructions: str = Fo
     with open(input_pdf_path, "wb") as buffer:
         shutil.copyfileobj(file.file, buffer)
 
-    redact_pdf(input_pdf_path, output_pdf_path, redact_instructions)
+    result = redact_pdf(input_pdf_path, output_pdf_path, redact_instructions)
 
     os.remove(input_pdf_path)  # Remove the original file
     
-    returnValue = {"filename": f"redacted_{file.filename}"}
+    returnValue = {"filename": f"redacted_{file.filename}", 'redacted_strings': result['redacted_strings']}
 
     return returnValue
 
